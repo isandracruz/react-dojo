a19e5d289e825d0d4a2987fb375016e9417e2a5a
Dayan Perez <dayan.perez9012@gmail.com>
2026-05-01 10:12:52 -0300

    fix: update Discord URL in constants.ts (#46)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,7 +9,7 @@ export const EDITOR_THEMES_META: Record<EditorThemeId, EditorThemeMeta> = {
 }
 
 export const REPOSITORY = "drbarzaga/react-dojo"
-export const DISCORD_URL = "https://discord.gg/nAjvxeXGn"
+export const DISCORD_URL = "https://discord.gg/ymGswJ9sp3"
 
 export const STARS_KILO_THRESHOLD = 1000
 
